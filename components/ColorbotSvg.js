73a547ddeb7d303ef16f28731c69b922347cd9b1
Michael Deitcher <michaeldeitcher@gmail.com>
2019-08-21 16:57:43 -0600

    Retriggering animations
    adding host config for zeit.now

--- components/ColorbotSvg.js
+++
@@ -1,5 +0,0 @@
-function ColorbotSvg() {
-    return <svg src="/static/colorbot-xyz.svg"/>
-}
-
-export default ColorbotSvg
--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,44 @@
+import React, { Component } from 'react';
 import Link from 'next/link'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
+import ReactSVG from 'react-svg'
 
+const animationDelay = 9000;
+const restartTitleAnimation = () => {
+    var element = document.getElementById("main-text");
+    element.classList.remove("animate")
+    void element.offsetWidth; // trigger a DOM reflow
+    setTimeout(restartTitleAnimationStep2, 1000);
+}
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-}));
+const restartTitleAnimationStep2 = () => {
+    var element = document.getElementById("main-text");
+    element.classList.add("animate");
+    setTimeout(restartTitleAnimation, animationDelay);
+}
 
-const Header = props => {
-    const classes = useStyles();
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <img src="/static/colorbot-xyz.svg" style={{'max-width': '60%'}}/>
-            </Toolbar>
-        </AppBar>
-    );
+class Header extends Component {
+
+    componentDidMount() {
+        setTimeout(restartTitleAnimation, animationDelay);
+    }
+    
+    render() {
+        return (
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <ReactSVG src="/static/colorbot-xyz.svg" style={{maxWidth: '60%', paddingTop: '8px'}}/>
+                </Toolbar>
+            </AppBar>
+        );
+    }
 }
 
 export default Header;
