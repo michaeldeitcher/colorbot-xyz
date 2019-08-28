bdf56902ebb1506ad839057306a667ba498a5421
Michael Deitcher <michaeldeitcher@gmail.com>
2019-08-28 10:36:28 -0600

    tidying up

--- pages/projects/trackball-robot-sphero.js
+++ pages/projects/trackball-robot-sphero.js
@@ -3,10 +3,10 @@ import Link from 'next/link'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import ReactPlayer from 'react-player'
 
-const useStyles = makeStyles({
+const styles = theme => ({
     sectionContainer: {
         marginTop: '20px'
     },
@@ -14,14 +14,16 @@ const useStyles = makeStyles({
         '& img, & p': {
             margin: 'auto',
             padding: '10px 0',
-            maxWidth: '90vw'
+            [theme.breakpoints.down('md')]: {
+                maxWidth: '90vw'
+            },
         }
     }
 });
 
 
-const Page = () => {
-    const classes = useStyles();
+const Page = (props) => {
+    const { classes } = props;
     return (
     <>
     <Container maxWidth="md">
@@ -30,8 +32,9 @@ const Page = () => {
     </Container>
 
     <Container className={classes.sectionContainer}>
-        <Typography variant="h6" component="h6" className={classes.sectionContainer}></Typography>
-        <Typography variant="body1" component="p">.</Typography>
+        <Typography variant="h4" component="h6" className={classes.sectionContainer}>Project: Tower at the End of Time</Typography>
+        <Typography variant="body1" component="p">Roll over six bridges to make it to the Tower at the End of Time!</Typography>
+        <Typography variant="h5" component="body1">Emerge - ShowOff 5.0 - Time Travel - Science Fair and Talent Show - April 9, 2016</Typography>
         <Grid container>
             <Grid item xs={12} lg={5}>
                 <Grid container className={classes.imgContainer}>
@@ -40,18 +43,17 @@ const Page = () => {
             </Grid>
             <Grid item xs={12} lg={7}>
                 <Grid container className={classes.imgContainer}>
-                    <Typography variant="body1" component="p">Roll over six bridges to make it to the Tower at the End of Time!</Typography>
                     <ReactPlayer url='https://vimeo.com/356282422' style={{margin: '10px auto', maxWidth: '90vw'}}/>
-                    <Typography variant="body1" component="p">Emerge - ShowOff 5.0 - Time Travel - Science Fair and Talent Show</Typography>
-                    <Typography variant="body1" component="p">April 9, 2016</Typography>
                 </Grid>
             </Grid>
         </Grid>
     </Container>
 
     <Container className={classes.sectionContainer}>
-        <Typography variant="h6" component="h6" className={classes.sectionContainer}></Typography>
-        <Typography variant="body1" component="p">.</Typography>
+        <Typography variant="h4" component="h6" className={classes.sectionContainer}>Project: Volcano Run!</Typography>
+        <Typography variant="body1" component="p">Teams race around two active volcanoes!</Typography>
+        <Typography variant="h5" component="body1">Danger Carnival - Dinosaur Jungle - September 17, 2016</Typography>
+
         <Grid container>
             <Grid item xs={12} lg={5}>
                 <Grid container className={classes.imgContainer}>
@@ -60,10 +62,7 @@ const Page = () => {
             </Grid>
             <Grid item xs={12} lg={7}>
                 <Grid container className={classes.imgContainer}>
-                    <Typography variant="body1" component="p">Teams race around two active volcanoes!</Typography>
                     <ReactPlayer url='https://vimeo.com/184030516' style={{margin: '10px auto', maxWidth: '90vw'}}/>
-                    <Typography variant="body1" component="p">Danger Carnival - Dinosaur Jungle</Typography>
-                    <Typography variant="body1" component="p">September 17, 2016</Typography>
                 </Grid>
             </Grid>
         </Grid>
@@ -71,4 +70,4 @@ const Page = () => {
     </>
 )};
 
-export default Layout(Page);
+export default Layout(withStyles(styles)(Page));
